1ebe9f44e1137fbbaaa9a9f139e3af1fa92d1701
wws <no@no.no>
2024-08-27 02:35:15 +0800

    微调

--- sqliteonweb-no-op/src/main/java/io/github/skyhacker2/sqliteonweb/SQLiteOnWeb.java
+++ sqliteonweb-no-op/src/main/java/io/github/skyhacker2/sqliteonweb/SQLiteOnWeb.java
@@ -1,12 +1,14 @@
 package io.github.skyhacker2.sqliteonweb;
 
 import android.content.Context;
+import android.net.wifi.WifiManager;
 
 /**
  * Created by eleven on 16/4/3.
  */
 public class SQLiteOnWeb {
-    private Context mContext;
+    private final Context mContext;
+    private final int mPort;
     private static SQLiteOnWeb mSQLiteOnWeb;
 
     public static SQLiteOnWeb init(Context context) {
@@ -24,14 +26,22 @@ public class SQLiteOnWeb {
     }
 
     private SQLiteOnWeb(Context context) {
-        mContext = context;
+        this(context, 9000);
     }
 
     private SQLiteOnWeb(Context context, int port) {
         mContext = context;
+        mPort = port;
     }
 
     public void start() {
 
+    }
+
+    public String getIpAccess() {
+        WifiManager wifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+        int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
+        final String formatedIpAddress = String.format("%d.%d.%d.%d", (ipAddress & 0xff), (ipAddress >> 8 & 0xff), (ipAddress >> 16 & 0xff), (ipAddress >> 24 & 0xff));
+        return "http://" + formatedIpAddress + ":" + mPort;
     }
 }
